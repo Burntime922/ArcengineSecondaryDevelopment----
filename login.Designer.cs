77d72e5bbdb3d92d4532e576fd86d6b247dd931b
Burntime922 <2294503646@qq.com>
2023-07-18 10:19:27 +0800

    rivise project name

--- login.Designer.cs
+++ login.Designer.cs
@@ -40,40 +40,45 @@ namespace _2020114120王晨冲
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(143, 107);
+            this.label1.Location = new System.Drawing.Point(107, 86);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "账号：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(146, 236);
+            this.label2.Location = new System.Drawing.Point(110, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "密码：";
             // 
             // textuser
             // 
-            this.textuser.Location = new System.Drawing.Point(243, 96);
+            this.textuser.Location = new System.Drawing.Point(182, 77);
+            this.textuser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textuser.Name = "textuser";
-            this.textuser.Size = new System.Drawing.Size(298, 25);
+            this.textuser.Size = new System.Drawing.Size(224, 21);
             this.textuser.TabIndex = 2;
             // 
             // textpsw
             // 
-            this.textpsw.Location = new System.Drawing.Point(243, 225);
+            this.textpsw.Location = new System.Drawing.Point(182, 180);
+            this.textpsw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textpsw.Name = "textpsw";
-            this.textpsw.Size = new System.Drawing.Size(298, 25);
+            this.textpsw.Size = new System.Drawing.Size(224, 21);
             this.textpsw.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(243, 343);
+            this.button1.Location = new System.Drawing.Point(182, 274);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 18);
             this.button1.TabIndex = 4;
             this.button1.Text = "登录";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,9 +86,10 @@ namespace _2020114120王晨冲
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(440, 342);
+            this.button2.Location = new System.Drawing.Point(330, 274);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(56, 18);
             this.button2.TabIndex = 5;
             this.button2.Text = "退出";
             this.button2.UseVisualStyleBackColor = true;
@@ -91,17 +97,18 @@ namespace _2020114120王晨冲
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textpsw);
             this.Controls.Add(this.textuser);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "login";
-            this.Text = "登录2020114120王晨冲供水管网地理信息系统的功能模块设计与界面设计";
+            this.Text = "登录供水管网地理信息系统的功能模块设计与界面设计";
             this.Load += new System.EventHandler(this.login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
